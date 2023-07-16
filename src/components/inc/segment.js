7975ede6abe8ca4f732c2190a6b854b40d63bb5e
MilanPatel2003 <milanpatel6454@gmail.com>
2023-07-16 18:21:59 +0530

    segments responsiveness updates3

--- src/components/inc/segment.js
+++ src/components/inc/segment.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import "./segment.css";
-import thumbnail1 from "../images/Thumbnail1.jpg";
-import thumbnail2 from "../images/Thumbnail2.jpg";
-import thumbnail3 from "../images/Thumbnail3.jpg";
+// import thumbnail1 from "../images/Thumbnail1.jpg";
+// import thumbnail2 from "../images/Thumbnail2.jpg";
+// import thumbnail3 from "../images/Thumbnail3.jpg";
 
 
 
