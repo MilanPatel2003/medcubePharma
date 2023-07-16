49f7fed41b5f193a4211ecc339f69e7adf6d3432
MilanPatel2003 <milanpatel6454@gmail.com>
2023-07-16 18:36:17 +0530

    segments responsiveness updates4

--- src/components/inc/segment.js
+++ src/components/inc/segment.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import "./segment.css";
-// import thumbnail1 from "../images/Thumbnail1.jpg";
-// import thumbnail2 from "../images/Thumbnail2.jpg";
-// import thumbnail3 from "../images/Thumbnail3.jpg";
+import thumbnail1 from "../images/Thumbnail1.jpg";
+import thumbnail2 from "../images/Thumbnail2.jpg";
+import thumbnail3 from "../images/Thumbnail3.jpg";
 
 
 
@@ -16,11 +16,61 @@ function Segment() {
     });
   }, []);
     return(
-        <div className="container"></div>
+        <div className="container"> 
+
+        <section className="py-5 text-center container">
+    <div className="row py-lg-5">
+      <div className="col-lg-6 col-md-8 mx-auto">
+        <h1 className="fw-light">Products Segment</h1>
+      </div>
+    </div>
+  </section>
+
+  <div className="container mb-4">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="thumbnail">
+            <Link to="/pharmaceuticals">
+              <img src={thumbnail1} alt="Thumbnail 1"  />
+              <div className="caption">
+                <h3>Pharmaceuticals</h3>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="col-md-4" >
+          <div className="thumbnail">
+            <Link to="/nutraceuticals">
+              <img src={thumbnail2} alt="Thumbnail 2"  />
+              <div className="caption">
+                <h3>Nutraceuticals</h3>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="col-md-4" >
+          <div className="thumbnail">
+            <Link to="/surgical">
+              <img src={thumbnail3} alt="Thumbnail 3" />
+              <div className="caption">
+                <h3>Surgical</h3>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
 
-  
+
+
+
+
+
+
+
+  </div>
     )
 }
 
