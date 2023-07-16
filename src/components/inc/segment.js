6bd54e043076423db63c41cccbd28f888ab87b8c
MilanPatel2003 <milanpatel6454@gmail.com>
2023-07-16 18:20:23 +0530

    segments responsiveness updates3

--- src/components/inc/segment.js
+++ src/components/inc/segment.js
@@ -16,61 +16,11 @@ function Segment() {
     });
   }, []);
     return(
-        <div className="container"> 
-
-        <section className="py-5 text-center container">
-    <div className="row py-lg-5">
-      <div className="col-lg-6 col-md-8 mx-auto">
-        <h1 className="fw-light">Products Segment</h1>
-      </div>
-    </div>
-  </section>
-
-  <div className="container mb-4">
-      <div className="row">
-        <div className="col-md-4" data-aos="fade-right">
-          <div className="thumbnail">
-            <Link to="/pharmaceuticals">
-              <img src={thumbnail1} alt="Thumbnail 1"  />
-              <div className="caption">
-                <h3>Pharmaceuticals</h3>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="col-md-4" >
-          <div className="thumbnail" data-aos="fade">
-            <Link to="/nutraceuticals">
-              <img src={thumbnail2} alt="Thumbnail 2"  />
-              <div className="caption">
-                <h3>Nutraceuticals</h3>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="col-md-4" data-aos="fade-left">
-          <div className="thumbnail">
-            <Link to="/surgical">
-              <img src={thumbnail3} alt="Thumbnail 3" />
-              <div className="caption">
-                <h3>Surgical</h3>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+        <div className="container"></div>
 
 
 
-
-
-
-
-
-
-
-  </div>
+  
     )
 }
 
