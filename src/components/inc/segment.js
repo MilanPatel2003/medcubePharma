cdbd5177871f4c234f65832d4024c2d1aa225178
MilanPatel2003 <milanpatel6454@gmail.com>
2023-07-17 12:38:07 +0530

    about us page added

--- src/components/inc/segment.js
+++ src/components/inc/segment.js
@@ -7,71 +7,59 @@ import thumbnail1 from "../images/Thumbnail1.jpg";
 import thumbnail2 from "../images/Thumbnail2.jpg";
 import thumbnail3 from "../images/Thumbnail3.jpg";
 
-
-
 function Segment() {
   useEffect(() => {
     AOS.init({
-      duration:2000,
+      duration: 2000,
     });
   }, []);
-    return(
-        <div className="container"> 
 
-        <section className="py-5 text-center container">
-    <div className="row py-lg-5">
-      <div className="col-lg-6 col-md-8 mx-auto">
-        <h1 className="fw-light">Products Segment</h1>
+  return (
+    <div className="container">
+      <section className="py-5 text-center">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8 mx-auto">
+            <h1 className="fw-light">Products Segment</h1>
+          </div>
+        </div>
+      </section>
+
+      <div className="container mb-4">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="thumbnail">
+              <Link to="/Pharmaceutical">
+                <img src={thumbnail1} alt="Thumbnail 1"  data-aos="fade-right" />
+                <div className="caption">
+                  <h3>Pharmaceuticals</h3>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="thumbnail" >
+              <Link to="/Nutraceutical">
+                <img src={thumbnail2} alt="Thumbnail 2" data-aos="fade" />
+                <div className="caption">
+                  <h3>Nutraceuticals</h3>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="thumbnail" >
+              <Link to="/Surgical">
+                <img src={thumbnail3} alt="Thumbnail 3" data-aos="fade-left" />
+                <div className="caption">
+                  <h3>Surgical</h3>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </section>
-
-  <div className="container mb-4">
-      <div className="row">
-        <div className="col-md-4">
-          <div className="thumbnail">
-            <Link to="/pharmaceuticals">
-              <img src={thumbnail1} alt="Thumbnail 1" data-aos="fade-right" />
-              <div className="caption">
-                <h3>Pharmaceuticals</h3>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="col-md-4" >
-          <div className="thumbnail">
-            <Link to="/nutraceuticals">
-              <img src={thumbnail2} alt="Thumbnail 2" data-aos="fade"  />
-              <div className="caption">
-                <h3>Nutraceuticals</h3>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="col-md-4" >
-          <div className="thumbnail">
-            <Link to="/surgical">
-              <img src={thumbnail3} alt="Thumbnail 3" data-aos="fade-left"/>
-              <div className="caption">
-                <h3>Surgical</h3>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-  </div>
-    )
+  );
 }
 
 export default Segment;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,77 @@
-import React, { Component } from "react";
+import {React, useEffect }from "react";
+import heroImage1 from "../images/About1.jpg";
+import heroImage2 from "../images/vision.jpg";
+import approach from "../images/approach.png";
+import gmpLogo from "../images/gmp.png";
+import "./About.css"
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
-class About extends Component{
-    render(){
-        return(
-            <div> 
-                <h2>this is About page</h2> 
-                <button className="btn btn-primary"> button</button>
+function About() {
+    useEffect(() => {
+        AOS.init({
+          duration:2000,
+        });
+      }, []);
+
+  return (
+    <div className="about-container">
+
+      <section className="py-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <img src={heroImage1} alt="Hero1" height="350px" width="400px" data-aos="fade-right" />
             </div>
-        )
-    }
+            <div className="col-md-6" data-aos="fade-left">
+              <h2>What We Do?</h2>
+              <p className="lead">
+At Medcube Pharmaceuticals, we specialize in trusted third-party pharma manufacturing. With state-of-the-art facilities and a dedicated team, we deliver high-quality pharmaceutical products. Our comprehensive manufacturing capabilities cover formulation development, production, and packaging. Partner with us for reliable and efficient manufacturing solutions that meet the highest industry standards.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
+      <div className="px-4 py-5 my-5 text-center" data-aos="fade">
+    <img className="d-block mx-auto mb-4" src={gmpLogo} alt="gmp-logo" height={"150px"}/>
+    <h1>WHO-GMP Certified</h1>
+    <div className="col-lg-6 mx-auto">
+      <p className="lead mb-4">We take pride in being WHO-GMP certified, ensuring that our manufacturing processes adhere to the highest industry standards. Our state-of-the-art facilities and dedicated team of experts enable us to deliver pharmaceutical products of the highest quality.
+      </p>
+    </div>
+  </div>
+
+
+
+
+      <section className="py-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-6 order-md-2">
+              <img src={heroImage2} alt="Hero2" className="img-fluid" data-aos="fade-left" />
+            </div>
+            <div className="col-md-6 order-md-1" data-aos="fade-right">
+              <h2>Our Vision</h2>
+              <p className="lead">
+              To become a global leader in the pharmaceutical industry by continuously innovating and delivering advanced healthcare solutions. We strive to improve the quality of life for individuals worldwide by providing safe, effective, and affordable medications. With a focus on research and development, we aim to address unmet medical needs and contribute to the well-being of communities globally.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="px-4 py-5 my-5 text-center" data-aos="fade">
+    <img className="d-block mx-auto mb-4" src={approach} alt="gmp-logo" height={"180px"}/>
+    <h1>Our Approach</h1>
+    <div className="col-lg-6 mx-auto">
+      <p className="lead mb-4">At Medcube Pharmaceuticals, we are dedicated to making a positive impact on global healthcare. By combining scientific expertise, innovation, and a passion for improving lives, we are committed to driving positive change and creating a healthier future for all.
+      </p>
+    </div>
+  </div>
+
+    </div>
+  );
 }
+
 export default About;
