e23c5d2a65084bbaf4f23e7c0866b200d8e5070c
MilanPatel2003 <milanpatel6454@gmail.com>
2023-07-18 14:28:14 +0530

    contact us page added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,77 @@
-import React, { Component } from "react";
+import React from "react";
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import "./contact.css";
 
-class Contact extends Component{
-    render(){
+
+function Contact(){
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+        .then((result) => {
+            console.log(result.text);
+            e.target.reset();
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
         return(
-            <div> 
-                <h2>this is Contact page</h2> 
-                <button className="btn btn-primary"> button</button>
+            <div className="contact-container shadow ">
+            <div className="row ">
+                <div className="col-md-4 bg-left p-5 text-white order-sm-first order-last">
+                    <h2>Let's get in touch</h2>
+                    <p>Feel free to reach out to us for any inquiries or questions. Our team is here to assist you!</p>
+                    <div className="d-flex mt-2">
+                        <i className="bi bi-geo-alt"></i>
+                        <p className="mt-3 ms-3">Address : Ahemdabad</p>
+                    </div>
+                    <div className="d-flex mt-2">
+                        <i className="bi bi-telephone-forward"></i>
+                        <p className="mt-4 ms-3">Phone : 8888888888</p>
+                    </div>
+                    <div className="d-flex mt-2">
+                        <i className="bi bi-envelope"></i>
+                        <p className="mt-4 ms-3">MedcubePharmaceuticals@gmail.com</p>
+                    </div>
+                    <div className="d-flex mt-2">
+                        <i className="bi bi-youtube"></i>
+                        <p className="mt-4 ms-3">Website : www.website.com/</p>
+                    </div>
+                </div>
+                <div className="col-md-8 p-5 ">
+                    <h2>Get in touch</h2>
+                    <form className="row g-3 contactForm mt-4" ref={form} onSubmit={sendEmail}>
+                        <div className="col-12">
+                          <label htmlFor="inputEmail4" className="form-label">Name</label>
+                          <input type="text" className="form-control" id="inputEmail4" required name="user_name" />
+                        </div>
+   
+                        <div className="col-12">
+                          <label htmlFor="inputAddress" className="form-label">Email Id</label>
+                          <input type="email" className="form-control" id="inputAddress" required name="user_email" />
+                        </div>
+                        <div className="col-12">
+                            <label htmlFor="inputAddress" className="form-label">Contact Number</label>
+                            <input type="tel" className="form-control" id="inputAddress" placeholder="optional" name="user_contact" />
+                          </div>
+                        <div className="col-12">
+                            <label htmlFor="inputCity" className="form-label">Message</label>
+                            <textarea type="text" className="form-control" id="inputCity" required name="user_message" /> 
+                          </div>
+                        <div className="col-12">
+                          <button type="submit" className="btn btn-primary mt-3">Send</button>
+                        </div>
+                      </form>
+                    
+                </div>
             </div>
+        </div>
         )
-    }
+    
 
 }
 export default Contact;
